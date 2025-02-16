bd45444e767615449bcee60598ec1235429c532d
buru0003 <buru0003@algonquinlive.com>
2025-02-15 23:55:54 -0500

    feat: Confirm the Nic is connected to VM and VM Version is correct.

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -2,7 +2,6 @@ package test
 
 import (
 	"testing"
-
 	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
@@ -10,7 +9,8 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-azure-subscription-id"
+var subscriptionID string = "286a69d3-dc09-45e3-b4a1-1b7dc9a02f90"
+
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-id>",
+			"labelPrefix": "buru0003vmTest",
 		},
 	}
 
@@ -28,9 +28,22 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 	terraform.InitAndApply(t, terraformOptions)
 
 	// Run `terraform output` to get the value of output variable
-	vmName := terraform.Output(t, terraformOptions, "vm_name")
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
+	vmName := terraform.Output(t, terraformOptions, "vm_name")
+	vmNicName := terraform.Output(t, terraformOptions, "nic_name")
 
 	// Confirm VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+
+	// Check if the NIC is available and connected to the specified VM
+	virtualMachineNics := azure.GetVirtualMachineNics(t, vmName, resourceGroupName, subscriptionID)
+	assert.Equal(t, vmNicName, virtualMachineNics[0])
+
+	// Check if the VM is running the correct version of Ubuntu
+	vmImage := azure.GetVirtualMachineImage(t, vmName, resourceGroupName, subscriptionID)
+	vmOsSKU := "22_04-lts-gen2"
+	vmOsPublisher := "Canonical"
+	assert.Equal(t, vmOsSKU, vmImage.SKU)
+	assert.Equal(t, vmOsPublisher, vmImage.Publisher)
+
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 # Define config variables
 variable "labelPrefix" {
   type        = string
-  description = "Your college username. This will form the beginning of various resource names."
+  description = "This will form the beginning of various resource names."
 }
 
 variable "region" {
-  default = "westus3"
+  default = "canadacentral"
 }
 
 variable "admin_username" {
